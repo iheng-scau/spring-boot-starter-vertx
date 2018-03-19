e146fce885689017c04e12c20f9146702405e8f0
iheng <iheng_scau@hotmail.com>
2018-03-19 18:58:05 +0800

    fix error caused by bug fixing

---
+++ spring-boot-vertx-mysql-starter/src/main/java/cn/iheng/springboot/starter/autoconfig/VertxMySqlAutoConfiguration.java
@@ -0,0 +1,12 @@
+package cn.iheng.springboot.starter.autoconfig;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author iheng
+ * @date 1/26/18
+ */
+@Configuration
+public class VertxMySqlAutoConfiguration {
+
+}
